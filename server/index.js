6317415f10b7e870d433532d537eb542a6fdb512
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-06-03 20:56:14 +0530

    AI Chat setup

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import { Configuration, OpenAIApi } from "openai";
+import openAiRoutes from "./routes/openai.js";
 
 
 /* CONFIGURATIONS */
@@ -25,7 +26,8 @@ const configuration = new Configuration({
 });
 export const openai = new OpenAIApi(configuration);
 
-
+/* ROUTES */
+app.use("/openai", openAiRoutes);
 
 /* SERVER SETUP */
 const PORT = process.env.PORT || 9000;
